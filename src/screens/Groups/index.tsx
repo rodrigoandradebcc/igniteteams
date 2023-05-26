cab552037604f83d215dd44529685b118dfbe523
Rodrigo Carneiro Andrade <rodrigoandradebcc@Rodrigos-iMac.local>
2023-05-26 00:24:26 -0300

    chore: add styled-component in react native

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+
+import * as S from './styles';
 
 export default function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <S.Container>
+      <S.Title>Groups</S.Title>
+    </S.Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
